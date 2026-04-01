32c05d8008d303011a80960703b1bd7a83c04f85
eukevytos <eukevytos@gmail.com>
2026-03-31 21:26:05 -0300

    criei os graficos, estilizei e lacei o js

--- script.js
+++ script.js
@@ -19,3 +19,25 @@ var menuSide = document.querySelector('.menu-lateral')
 btnExp.addEventListener('click', function(){
     menuSide.classList.toggle('expandir')
 })
+
+const ctx = document.getElementById('grafico');
+
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+    
